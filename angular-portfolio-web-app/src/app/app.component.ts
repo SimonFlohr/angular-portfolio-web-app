5994ec905a4b35b998bb5aa0665c516c17a99445
Simon Flohr <sflohr2@wgu.edu>
2025-02-27 04:43:59 -0600

    feat(component): create `SocialsComponent` using `ng g c`
    - Generate `SocialsComponent` in 'component/footer/socials'.
    - Import `SocialsComponent` in 'app.component.ts'.
    - Apply additional Tailwind CSS classes and create divs to prepare layout for `SocialsComponent`.

--- angular-portfolio-web-app/src/app/app.component.ts
+++ angular-portfolio-web-app/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TitleComponent } from './component/navbar/title/title.component';
 import { NavDesktopComponent } from "./component/navbar/nav-desktop/nav-desktop.component";
+import { SocialsComponent } from './component/footer/socials/socials.component';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +10,8 @@ import { NavDesktopComponent } from "./component/navbar/nav-desktop/nav-desktop.
   imports: [
     CommonModule,
     TitleComponent,
-    NavDesktopComponent
+    NavDesktopComponent,
+    SocialsComponent
 ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'

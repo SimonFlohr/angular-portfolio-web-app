e769d456a431c29d32ddaa1d9872f4c7aed97df9
Simon Flohr <sflohr2@wgu.edu>
2025-02-26 21:17:22 -0600

    feat(component): create `NavDesktopComponent` using `ng g c`
    - Generate `NavDesktopComponent`.
    - Import the new component in 'app.component.ts'.
    - Apply basic Tailwind CSS layout classes to `<app-nav-desktop>` in 'app.component.html'.

--- angular-portfolio-web-app/src/app/app.component.ts
+++ angular-portfolio-web-app/src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TitleComponent } from './component/navbar/title/title.component';
+import { NavDesktopComponent } from "./component/navbar/nav-desktop/nav-desktop.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     CommonModule,
-    TitleComponent
-  ],
+    TitleComponent,
+    NavDesktopComponent
+],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

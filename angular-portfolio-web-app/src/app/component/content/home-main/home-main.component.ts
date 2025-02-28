6592a3e0724e4ea0dcb410527947f254706336d8
Simon Flohr <sflohr2@wgu.edu>
2025-02-28 04:18:54 -0600

    feat(component): construct the `HomeMainComponent`
    - Create component layout using divs and Tailwind CSS classes in 'home-main.component.html'.
    - Import `GreekStatueComponent` in 'home-main.component.ts'.
    - Add and remove a few Tailwind CSS classes from `<app-home-main>` and the top-level `<div>`.

--- angular-portfolio-web-app/src/app/component/content/home-main/home-main.component.ts
+++ angular-portfolio-web-app/src/app/component/content/home-main/home-main.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { GreekStatueComponent } from "../greek-statue/greek-statue.component";
 
 @Component({
   selector: 'app-home-main',
   standalone: true,
-  imports: [],
+  imports: [GreekStatueComponent],
   templateUrl: './home-main.component.html',
   styleUrl: './home-main.component.css'
 })

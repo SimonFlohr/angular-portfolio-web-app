76d074101ffd015e6d4a9033cd27046b84efccff
Simon Flohr <sflohr2@wgu.edu>
2025-02-28 02:33:28 -0600

    feat(component): create `HomeMainComponent` component
    - Generate `HomeMainComponent` component in 'component/content/home-main'.
    - Import the component in 'app.component.ts'.
    - Adjust the page layout to include a main content section.

--- angular-portfolio-web-app/src/app/app.component.ts
+++ angular-portfolio-web-app/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { TitleComponent } from './component/navbar/title/title.component';
 import { NavDesktopComponent } from "./component/navbar/nav-desktop/nav-desktop.component";
 import { SocialsComponent } from './component/footer/socials/socials.component';
+import { HomeMainComponent } from './component/content/home-main/home-main.component';
 
 @Component({
   selector: 'app-root',
@@ -11,7 +12,8 @@ import { SocialsComponent } from './component/footer/socials/socials.component';
     CommonModule,
     TitleComponent,
     NavDesktopComponent,
-    SocialsComponent
+    SocialsComponent,
+    HomeMainComponent
 ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'

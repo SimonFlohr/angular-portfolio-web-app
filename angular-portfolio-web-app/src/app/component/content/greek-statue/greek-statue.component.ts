03af3442b9b767688d112696366639dcaa5674a8
Simon Flohr <sflohr2@wgu.edu>
2025-02-28 20:30:54 -0600

    feat(anime.js): create the `animateComposition()` animation with anime.js in 'greek-statue.component.ts'

--- angular-portfolio-web-app/src/app/component/content/greek-statue/greek-statue.component.ts
+++ angular-portfolio-web-app/src/app/component/content/greek-statue/greek-statue.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import anime from 'animejs';
 
 @Component({
   selector: 'app-greek-statue',
@@ -7,6 +8,48 @@ import { Component } from '@angular/core';
   templateUrl: './greek-statue.component.html',
   styleUrl: './greek-statue.component.css'
 })
-export class GreekStatueComponent {
+export class GreekStatueComponent implements AfterViewInit {
+  ngAfterViewInit(): void {
+    this.animateComposition();
+  }
 
+  animateComposition(): void {
+    const circle = document.getElementById('circle');
+    const clouds = ['cloud-one', 'cloud-two', 'cloud-three'].map(id => document.getElementById(id));
+
+    if (circle) {
+      circle.style.transform = 'scale(0.3)';
+      circle.style.translate = '0 210px';
+
+      anime({
+        targets: '#circle',
+        scale: [0.3, 1],
+        translateY: [-200],
+        duration: 4000,
+        easing: 'easeInOutExpo',
+        delay: 500
+      });
+    }
+
+    clouds.forEach((cloud, index) => {
+      if (cloud) {
+        // Set initial position
+        anime.set(cloud, {
+          translateX: 0
+        });
+        
+        // create continuous cloud animation
+        anime({
+          targets: cloud,
+          translateX: [-30, 30],
+          duration: 3000,
+          direction: 'alternate',
+          loop: true,
+          easing: 'easeInOutSine',
+          delay: index * 800,
+          endDelay: 0
+        });
+      }
+    });
+  }
 }
